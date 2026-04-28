345d64a103d63ea546c1902622720d168df748e7
Tanaka Makuvaza <tmaku12430@gmail.com>
2026-04-28 16:04:15 -0400

    feat(app): register FCM background message handler before runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app/app.dart';
+import 'features/notifications/fcm_service.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -10,5 +12,8 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Background handler must be registered before runApp so FCM can invoke
+  // it in a fresh isolate while the app is killed.
+  FirebaseMessaging.onBackgroundMessage(vibzcheckFcmBackgroundHandler);
   runApp(const ProviderScope(child: VibzcheckApp()));
 }
